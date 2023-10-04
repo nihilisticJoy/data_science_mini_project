60f0c0c09896ccc8d25265d35a07a923c6e022e8
Hanieh Heidarpour <h.heidarpour73@gmail.com>
2023-10-04 21:12:34 +0300

    Final dataset

--- steam_game_analysis/src_data/data_cleaner.py
+++ steam_game_analysis/src_data/data_cleaner.py
@@ -1,10 +1,16 @@
-import numpy as np
 import pandas as pd
 import time
 
-FILE_PATH = "../result_5_tags.csv"
+FILE_PATH = "../result_6_tags.csv"
 data = pd.read_csv(FILE_PATH)
 
+# remove all Nas in title (5 cases)
+data.dropna(subset=['Title'], inplace=True)
+
+# remove all Nas in title (1 case)
+data.dropna(subset=['Platforms'], inplace=True)
+
+# remove all Nas in released
 data.dropna(subset=['Released'], inplace=True)
 
 print(data.isnull().sum())
@@ -45,7 +51,7 @@ data['Discount'].fillna(100, inplace=True)
 # create new feature `Day_since_release` extracted from `Release`
 def count_days_since_release(release_date):
     print(release_date)
-    formats = ["%d %b, %Y", "%Y %m %d", "%d %b %Y", "%b %d, %Y"]
+    formats = ["%d %b, %Y", "%Y %m %d", "%d %b %Y", "%b %d, %Y", "%Y", "%b %Y"]
     release_date = release_date.replace("févr.", "Feb,")
     release_date = release_date.replace("déc.", "Dec,")
     release_date = release_date.replace("oct.", "Oct,")
@@ -54,11 +60,35 @@ def count_days_since_release(release_date):
     release_date = release_date.replace("juil.", "Jul,")
     release_date = release_date.replace("juin", "Jun,")
     release_date = release_date.replace("sept.", "Sep,")
+    release_date = release_date.replace("nov.", "Nov,")
     release_date = release_date.replace("mars", "Mar,")
     release_date = release_date.replace("aout", "Aug,")
     release_date = release_date.replace("mai", "May,")
+    release_date = release_date.replace("January", "Jan")
+    release_date = release_date.replace("February", "Feb")
     release_date = release_date.replace("March", "Mar")
-    release_date = release_date.replace("Coming soon", "15 May, 2024")
+    release_date = release_date.replace("April", "Apr")
+    release_date = release_date.replace("June", "Jun")
+    release_date = release_date.replace("July", "Jul")
+    release_date = release_date.replace("August", "Aug")
+    release_date = release_date.replace("September", "Sep")
+    release_date = release_date.replace("October", "Oct")
+    release_date = release_date.replace("December", "Dec")
+    release_date = release_date.replace("November", "Nov")
+    if release_date.find("ᵉ\xa0trimestre\xa0") == 1:
+        release_date = release_date.replace("ᵉ\xa0trimestre\xa0", " ")
+        release_date = "Q" + release_date
+    release_date = release_date.replace("ᵉ\xa0trimestre\xa0", "Q")
+    release_date = release_date.replace("Q1", "Jan")
+    release_date = release_date.replace("Q2", "Apr")
+    release_date = release_date.replace("Q3", "Jul")
+    release_date = release_date.replace("Q4", "Oct")
+    release_date = release_date.replace("à déterminer", "To be announced")
+    release_date = release_date.replace("Prochainement", "Coming soon")
+    release_date = release_date.replace("Coming Soon", "Coming soon")
+    release_date = release_date.replace("Coming soon", "2024")
+    release_date = release_date.replace("To be announced", "2024")
+
     try_date = release_date
     for f in formats:
         try:
@@ -70,6 +100,9 @@ def count_days_since_release(release_date):
                try_date = "15 " + release_date
            elif len(elements) == 6:
                 try_date = f"{elements[0]} {elements[2]} {elements[4]}"
+        except OverflowError:
+            release_time = 0
+            break
 
     return int((time.time() - release_time) / (24 * 60 * 60))
 
@@ -94,8 +127,10 @@ on_sale = [is_on_sale(v) for v in data['Discount']]
 data.insert(11, "On_sale", on_sale, True)
 
 
+print(data.isnull().sum())
+
 # export to csv file
-data.to_csv("result_6.csv")
+data.to_csv("clean_data.csv")
 
 
 
