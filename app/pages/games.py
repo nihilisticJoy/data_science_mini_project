6c3c7ad19d5b79ad2e035899e1b69e28ad91b526
Akihiro Tanaka <akihiro.fin@gmail.com>
2023-10-28 00:29:45 +0300

    Added a data table to explore

--- app/pages/games.py
+++ app/pages/games.py
@@ -1,12 +1,26 @@
 import dash
-from dash import html
+from dash import html, dash_table, Output, Input, callback
+import pandas as pd
 
 dash.register_page(__name__, path="/games")
 
+FILE_PATH = "steam_game_analysis/src_data/clean_data.csv"
+df = pd.read_csv(FILE_PATH)
+
 layout = html.Div(
     [
-        html.H1("This is our Games page"),
-        html.Div("This is our Games page content."),
+        html.H1("Games"),
+        html.P("Explore our dataset!"),
+        dash_table.DataTable(
+            id="table",
+            columns=[{"name": c, "id": c, "hideable": True} for c in df.columns[1:]],
+            data=df.to_dict("records"),
+            editable=False,
+            page_action="native",
+            page_size=20,
+            sort_action="native",
+            style_as_list_view=True,
+        ),
     ],
     className="content",
 )
