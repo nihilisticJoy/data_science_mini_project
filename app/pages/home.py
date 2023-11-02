679914006750824ebb4d97ad0005e27ff6a40dac
Akihiro Tanaka <akihiro.fin@gmail.com>
2023-11-02 17:24:16 +0200

    Update home page

--- app/pages/home.py
+++ app/pages/home.py
@@ -5,8 +5,11 @@ dash.register_page(__name__, path="/")
 
 layout = html.Div(
     [
-        html.H1("This is our Home page"),
-        html.Div("This is our Home page content."),
+        html.H1("Gamesaver"),
+        html.P(
+            """Welcome to Gamesaver!
+            Explore the dataset freely and create your own machine learning models to predict insights related to the games on Stream!"""
+        ),
     ],
     className="content",
 )
